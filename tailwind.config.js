61a03b6a636bbeaea4b1d491cabdd4d62d38bd3b
mirrortheorypod-ux <mirrortheorypod@gmail.com>
2026-04-07 19:46:45 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}"
-  ],
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
